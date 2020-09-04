6e782aab95da3b23bcac90cd81ce99edc3ea7748
azizalfort <60493351+azizalfort@users.noreply.github.com>
2020-09-04 16:35:53 -0700

    Update Conversion.java

--- assignmentPR/Conversion.java
+++ assignmentPR/Conversion.java
@@ -1,1 +1,40 @@
 
+public class Conversion
+{
+    int characteristic;
+    int numerator;
+    int denominator;  
+    
+    String number = "123.456";
+
+     
+    public void conversion() {
+    //if the conversion from C string to integers can take place
+    if(characteristic(number, characteristic) && mantissa(number, numerator, denominator))
+    {
+        //do some math with c, n, and d
+    }
+    else
+    {
+        //handle the error on input
+    }
+    
+   
+    }
+    
+    
+    
+    /* description here*/
+    public boolean characteristic(char numString[], int charateristic)
+    {
+        
+    return true;
+    }
+
+    /*description here */
+    public boolean mantissa(char numString[], int numerator, int denominator)
+    {
+        
+    return true;
+    }
+}
